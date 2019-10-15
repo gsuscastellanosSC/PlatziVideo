d064dc155fd29e2a175bf3bafc2ed35bbdf66d1c
SC <jesuscastellanospaez@hotmail.com>
2019-10-14 19:34:30 -0500

    Webpack Dev Server: Reporte de errores y Cambios en tiempo real

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,7 +1,8 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
 
-const HelloWord = ()=>{
-    <h1>Hello Word</h1>
-}
+const HelloWord = () => (
+  <h1>Hello Word</h1>
+);
 
 export default HelloWord;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import HelloWord from './components/HelloWorld';
 
-ReactDOM.render(<HelloWord/>, document.getElementById("app"));
+ReactDOM.render(<HelloWord/>, document.getElementById('app'));
