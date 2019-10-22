6cdcb3ebe66caf37169de71558387c34c0e04059
SC <jesuscastellanospaez@hotmail.com>
2019-10-22 04:56:36 -0500

    Instalación de Redux Vamos a instalar las dependencias para poder trabajar con Redux:
    npm install redux react-redux --save
    Dentro de nuestro proyecto vamos a crear una carpeta para nuestros actions y otra para los reducers que utilizaremos en Redux.
    
    El paquete react-redux nos proporciona un Provider para poder encapsular nuestros componentes por medio de un connect para poder transmitir la información que necesitemos del store a cada componente.
    https://platzi.com/clases/1652-react-router-redux/22136-instalacion-de-redux/

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+// eslint-disable-next-line no-unused-vars
+import { createStore } from 'redux';
 import App from './routes/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById('app'),
+);
