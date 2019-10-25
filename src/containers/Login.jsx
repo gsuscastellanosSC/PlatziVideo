12ee87338fb880295331f3d62a7a3036355ce346
SC <jesuscastellanospaez@hotmail.com>
2019-10-24 21:09:35 -0500

    Crear Formulario de Login Clase 16 https://platzi.com/clases/1652-react-router-redux/22141-crear-formulario-de-login/

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,9 +1,10 @@
+/* eslint-disable no-console */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/jsx-closing-tag-location */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable react/button-has-type */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../assets/styles/components/Login.scss';
 
@@ -11,34 +12,69 @@ import googleIcon from '../assets/static/google-icon.png';
 import twitterIcon from '../assets/static/twitter-icon.png';
 
 
-const Login = () => (
-  <section className="login">
-    <section className="login__container">
-      <h2>Inicia sesión</h2>
-      <form className="login__container--form">
-        <input aria-label="Correo" className="input" type="text" placeholder="Correo" />
-        <input aria-label="Contraseña" className="input" type="password" placeholder="Contraseña" />
-        <button className="button">Iniciar sesión</button>
-        <div className="login__container--remember-me">
-          <label>
-            <input type="checkbox" id="cbox1" value="checkbos" />Recuérdame
-          </label>
-          <a href="/">Olvidé mi contraseña</a>
-        </div>
-      </form>
-      <section className="login__container--social-media">
-        <div>
-          <img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
-        <div>
-          <img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
-      </section>
-      <p className="login__container--register">No tienes ninguna cuenta
-        <Link to="/Register">
-          Regístrate
-        </Link>
-      </p>
-    </section>
-  </section>
-);
+const Login = () => {
+  const [form, setValues] = useState({
+    email: '',
+  });
 
+  const handleInput = (event) => {
+    setValues({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const hadleSubmit = (event) => {
+    event.preventDefault();
+    console.log(form);
+  };
+
+  return (
+    <section className="login">
+      <section className="login__container">
+        <h2>Inicia sesión</h2>
+        <form className="login__container--form" onSubmit={hadleSubmit}>
+          <input
+            name="email"
+            aria-label="Correo"
+            className="input"
+            type="text"
+            placeholder="Correo"
+            onChange={handleInput}
+          />
+          <input
+            name="password"
+            aria-label="Contraseña"
+            className="input"
+            type="password"
+            placeholder="Contraseña"
+            onChange={handleInput}
+          />
+          <button className="button">Iniciar sesión</button>
+          <div className="login__container--remember-me">
+            <label>
+              <input
+                type="checkbox"
+                id="cbox1"
+                value="checkbos"
+              />Recuérdame
+            </label>
+            <a href="/">Olvidé mi contraseña</a>
+          </div>
+        </form>
+        <section className="login__container--social-media">
+          <div>
+            <img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
+          <div>
+            <img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
+        </section>
+        <p className="login__container--register">No tienes ninguna cuenta
+          <Link to="/Register">
+            Regístrate
+          </Link>
+        </p>
+      </section>
+    </section>
+  );
+};
 export default Login;
