2e4e2fa23dfb79215f5b127cd70713d9f0365a7b
SC <jesuscastellanospaez@hotmail.com>
2019-10-24 22:28:14 -0500

    Validación para LogIn LogOut Clase 20 https://platzi.com/clases/1652-react-router-redux/22145-validacion-para-login-logout/

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
+import { logoutRequest } from '../actions';
 import '../assets/styles/components/Header.scss';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
@@ -11,6 +12,11 @@ import userIcon from '../assets/static/user-icon.png';
 const Header = (props) => {
   const { user } = props;
   const hasUser = Object.keys(user).length > 0;
+
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
+
   return (
     <header className="header">
 
@@ -27,8 +33,10 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li><a href="/">Cuenta</a></li>
-          <li><Link to="/Login">Iniciar sesión</Link></li>
+          {hasUser ? <li><a href="/">{user.name}</a></li> : null}
+          
+          {hasUser ? <li><a href="#logout" onClick={handleLogout}>Cerrar Sesión</a></li> : <li><Link to="/Login">Iniciar sesión</Link></li>}
+          
         </ul>
       </div>
     </header>
@@ -39,4 +47,8 @@ const mapStateToProps = (state) => ({
   user: state.user,
 });
 
-export default connect(mapStateToProps, null)(Header);
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
