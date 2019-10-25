14b3da208032cb389d0248eb8e976ad975afce27
SC <jesuscastellanospaez@hotmail.com>
2019-10-25 11:33:46 -0500

    Arreglando la funcionalidad del player Clase 24 https://platzi.com/clases/1652-react-router-redux/22149-arreglando-la-funcionalidad-del-player/

--- src/containers/Player.jsx
+++ src/containers/Player.jsx
@@ -1,19 +1,24 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
 import '../assets/styles/components/Player.scss';
 
-const Player = () => (
-  <div className="Player">
-    <video controls autoPlay>
-      <source src="" type="video/mp4" />
+const Player = (props) => {
+  const { id } = props.match.params;
+  return (
+    <div className="Player">
+      <video controls autoPlay>
+        <source src="" type="video/mp4" />
       Your browser does not support HTML5 video.
-    </video>
-    <div className="Player-black">
-      <button type="button">
+      </video>
+      <div className="Player-back">
+        <button type="button" onClick={() => props.history.goBack()}>
                 Regresar
-      </button>
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Player;
