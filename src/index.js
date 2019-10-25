1a44285b3d76ec8d7c3da18fab8e14d70233644d
SC <jesuscastellanospaez@hotmail.com>
2019-10-25 17:49:02 -0500

    Debug con Redux Devtools Clase 27 https://platzi.com/clases/examen/46c06b92-2b58-411c-a753-d6f23d6de1e0/examen_usuario/

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import reducer from './reducers';
 import App from './routes/App';
 
@@ -175,7 +175,8 @@ const initialState = {
     ],
 };
 
-const store = createStore(reducer, initialState);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
