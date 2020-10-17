05324d7db3644fab0ab1dde47a7e457753a96e60
Jesus Castellanos Paez <jesus.castellanos@solem.cl>
2020-10-17 02:07:37 -0500

    Insert: Insert: App in status Successful
    Signed-off-by: Jesus Castellanos Paez <jesus.castellanos@solem.cl>

---
+++ src/containers/Home.jsx
@@ -0,0 +1,54 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { connect } from 'react-redux';
+import Search from '../components/Search';
+import Categories from '../components/Categories';
+import Carousel from '../components/Carousel';
+import CarouserItem from '../components/CarouselItem';
+import '../assets/styles/Media.scss';
+import '../assets/styles/App.scss';
+
+const Home = ({ myList, trends, originals }) => {
+  return (
+    <>
+      <Search isHome />
+      {myList.length > 0
+      && (
+      <Categories title="Mi lista">
+        <Carousel>
+          {myList.map((item) => (
+            <CarouserItem
+              key={item.id}
+              {...item}
+              isList
+            />
+          ))}
+        </Carousel>
+      </Categories>
+      )}
+
+
+      <Categories title="Tendencias">
+        <Carousel>
+          {trends.map((item) => <CarouserItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+
+      <Categories title="Originales de PlatziVideo">
+        <Carousel>
+          {originals.map((item) => <CarouserItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+    </>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  myList: state.myList,
+  trends: state.trends,
+  originals: state.originals,
+});
+
+export default connect(mapStateToProps, null)(Home);
