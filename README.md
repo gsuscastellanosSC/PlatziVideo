57e4c787b925f9aaa956e0dbd638db8ed65ac43a
Jesus Castellanos Paez <jesus.castellanos@solem.cl>
2020-10-17 01:30:52 -0500

    Delete: All files
    Signed-off-by: Jesus Castellanos Paez <jesus.castellanos@solem.cl>

--- README.md
+++
@@ -1,5 +0,0 @@
-# PlatziVideo
-Aplicación platzivideo desarrollado para la Escuela de Javascript 2019 en @Platzi💚💚💚
-
-* npm install
-* npm run
--- movies-api/config/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-const config = {
-    dev: process.env.NODE_ENV !== 'production',
-    port: process.env.PORT || 4000
-}
-module.exports = { config };
